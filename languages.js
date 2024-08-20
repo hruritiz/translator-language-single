59ca185e5282cfebc206358c69f1cb0f8953152a
Hruritiz Corporation <hruritiz@gmail.com>
2024-08-20 17:39:20 +0300

    Add files via upload

---
+++ languages.js
@@ -0,0 +1,29 @@
+const userLang = navigator.language || navigator.userLanguage;
+const langCode = userLang.substring(0, 2);
+
+// Translations
+const translations = {
+    ru: {
+        welcome: 'Добро пожаловать',
+    },
+    uk: {
+        welcome: 'Ласкаво просимо',
+    },
+    pl: {
+        welcome: 'Witamy',
+    },
+    cs: {
+        welcome: 'Vítejte',
+    },
+};
+function applyTranslations(translations) {
+  document.querySelectorAll('[data-i18n]').forEach(element => {
+  const key = element.getAttribute('data-i18n');
+  if (translations[key]) {
+  if (element.tagName === 'INPUT' && element.hasAttribute('placeholder')) {
+  element.setAttribute('placeholder', translations[key]);}
+  else {element.innerHTML = translations[key];}}});}
+  window.addEventListener('DOMContentLoaded', () => {
+  applyTranslations(Object.values(translations).find((trans, i) => userLang.includes(Object.keys(translations)[i])));
+  document.body.classList.remove('loading');}
+);
